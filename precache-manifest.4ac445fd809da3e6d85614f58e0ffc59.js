06744342308ca0fca38c001d898eb855bf036cdf
dev at <battuosai01@gmail.com>
2020-06-15 14:53:03 -0700

    Updates

---
+++ precache-manifest.4ac445fd809da3e6d85614f58e0ffc59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e40fdc464d063c01db6aebec2a26109",
+    "url": "/todolist-reactjs/index.html"
+  },
+  {
+    "revision": "f2841b672fa75e01cae6",
+    "url": "/todolist-reactjs/static/css/main.c8d23c9d.chunk.css"
+  },
+  {
+    "revision": "1ecb67bbc2f6e0b3fd5a",
+    "url": "/todolist-reactjs/static/js/2.7591b362.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-reactjs/static/js/2.7591b362.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2841b672fa75e01cae6",
+    "url": "/todolist-reactjs/static/js/main.36ac8fba.chunk.js"
+  },
+  {
+    "revision": "606eae5481d6629efdf6",
+    "url": "/todolist-reactjs/static/js/runtime-main.c8e2119e.js"
+  }
+]);
