b3f1186f9c3872e0e5ad754647834a5b0d6e5122
dev at <battuosai01@gmail.com>
2020-06-15 14:46:57 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Form from './components/Form';
 import List from './components/List';
 
-
 class App extends React.Component{
   state = {
     data:[]
@@ -18,7 +17,9 @@ class App extends React.Component{
 
   componentDidMount(){
     const dataStore = JSON.parse(localStorage.getItem('dataStore'));
-    this.setState({data: dataStore});
+    if(dataStore !== null){
+      this.setState({data: dataStore});
+    }
   }
 
 
